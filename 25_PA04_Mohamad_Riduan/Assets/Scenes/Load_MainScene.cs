16c1cdf261fa23211dd6d9f296eda2e707ca1e67
Dasharupa <64124329+Dasharupa@users.noreply.github.com>
2020-05-05 16:29:59 +0800

    Added MainScene
    Added MainScene by Dasharupa

---
+++ 25_PA04_Mohamad_Riduan/Assets/Scenes/Load_MainScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_MainScene : MonoBehaviour
+{
+    public string SceneToLoad = "";
+    public float DelayTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Wait");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(DelayTime);
+        SceneManager.LoadScene("MainScene");
+    }
+}
